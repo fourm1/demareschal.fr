7c4ceb6d6e79bdd3f4374d8397c44f56e85490e7
ademareschal <toto.63146@gmail.com>
2024-09-19 17:22:55 +0200

    Début d'un truc, en sah de sah

--- front/src/components/HomePage.vue
+++ front/src/components/HomePage.vue
@@ -1,43 +1,58 @@
 <template>
-        <div class="home">
-                <header class="header">
-                        <h1>Antoine de Mareschal</h1>
-                        <h2>Développeur full-stack junior</h2>
-                </header>
+  <div class="home">
+    <header class="header">
+      <h1>Antoine de Mareschal</h1>
+      <h2>Développeur full-stack junior</h2>
+    </header>
 
-                <div class="carousel-container">
-                        <transition-group name="slide" tag="p" class="carousel">
-                                <span :key="currentTech" class="carousel-item">{{ currentTech }}</span>
-                        </transition-group>
-                </div>
-        </div>
+    <div class="carousel-container">
+      <!-- Utilisation d'une transition groupée pour coordonner l'apparition et la disparition -->
+      <transition
+          name="carousel"
+          @before-enter="beforeEnter"
+          @after-leave="afterLeave"
+          mode="out-in"
+      >
+        <span :key="currentTech" class="carousel-item">{{ currentTech }}</span>
+      </transition>
+    </div>
+  </div>
 </template>
 
 <script>
 
-import '@/assets/styles/home.scss';
+import('@/assets/styles/home.scss')
 
 export default {
-        data() {
-                return {
-                        techs: ['Vue', 'React', 'Angular', 'HTML', 'CSS', 'Javascript', 'Typescript', 'Git'],
-                        currentIndex: 0,
-                };
-        },
-        computed: {
-                currentTech() {
-                        return this.techs[this.currentIndex];
-                },
-        },
-        mounted() {
-                this.startCarousel();
-        },
-        methods: {
-                startCarousel() {
-                        setInterval(() => {
-                                this.currentIndex = (this.currentIndex + 1) % this.techs.length;
-                        }, 2000); // Change toutes les 2 secondes
-                },
-        },
+  data() {
+    return {
+      techs: ['Vue', 'React', 'Angular', 'HTML', 'CSS', 'Javascript', 'Typescript', 'Git'],
+      currentIndex: 0,
+      isAnimating: false, // Pour contrôler quand démarrer l'animation
+    };
+  },
+  computed: {
+    currentTech() {
+      return this.techs[this.currentIndex];
+    },
+  },
+  mounted() {
+    this.startCarousel();
+  },
+  methods: {
+    startCarousel() {
+      setInterval(() => {
+        if (!this.isAnimating) {
+          this.currentIndex = (this.currentIndex + 1) % this.techs.length;
+        }
+      }, 3000); // Change toutes les 3 secondes pour laisser le temps à l'animation
+    },
+    beforeEnter() {
+      this.isAnimating = true;
+    },
+    afterLeave() {
+      this.isAnimating = false;
+    }
+  },
 };
 </script>
