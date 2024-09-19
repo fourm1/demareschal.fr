fd9f1eaf93cc028fd9e3653c7265d3c10afaaa2c
ademareschal <toto.63146@gmail.com>
2024-09-19 15:23:32 +0200

    Tests divers et variés (vers et variés)

---
+++ front/src/components/HomePage.vue
@@ -0,0 +1,43 @@
+<template>
+        <div class="home">
+                <header class="header">
+                        <h1>Antoine de Mareschal</h1>
+                        <h2>Développeur full-stack junior</h2>
+                </header>
+
+                <div class="carousel-container">
+                        <transition-group name="slide" tag="p" class="carousel">
+                                <span :key="currentTech" class="carousel-item">{{ currentTech }}</span>
+                        </transition-group>
+                </div>
+        </div>
+</template>
+
+<script>
+
+import '@/assets/styles/home.scss';
+
+export default {
+        data() {
+                return {
+                        techs: ['Vue', 'React', 'Angular', 'HTML', 'CSS', 'Javascript', 'Typescript', 'Git'],
+                        currentIndex: 0,
+                };
+        },
+        computed: {
+                currentTech() {
+                        return this.techs[this.currentIndex];
+                },
+        },
+        mounted() {
+                this.startCarousel();
+        },
+        methods: {
+                startCarousel() {
+                        setInterval(() => {
+                                this.currentIndex = (this.currentIndex + 1) % this.techs.length;
+                        }, 2000); // Change toutes les 2 secondes
+                },
+        },
+};
+</script>
