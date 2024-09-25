a3ad3477aaf4dc8c8f2313a12897280736d95b6e
ademareschal <toto.63146@gmail.com>
2024-09-25 11:21:28 +0200

    Ajout de 6-7 trucs pour la page d'accueil

--- front/src/components/HomePage.vue
+++ front/src/components/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="home">
-        <div class="first-page">
+        <section class="first-page">
             <header class="header">
                 <h1>Antoine de Mareschal</h1>
                 <h2>Développeur full-stack junior</h2>
@@ -23,28 +23,23 @@
                 </transition>
             </div>
 
-            <div class="profile">
-                <p>(The laws of roads don't apply anymore when 4:55 of Free Bird starts playing.)</p>
+            <div class="cv">
+                <a href="@/assets/downloadable/jetbrains-toolbox-2.2.2.20062.exe" download class="cv-button">
+                    Télécharger mon cv
+                </a>
             </div>
 
             <div class="scroll-arrow" @click="scrollToNextSection">
                 &#x2193;
             </div>
-        </div>
+        </section>
 
-        <div class="second-page" id="ouais">
+        <section class="second-page" ref="jsp">
             <div class="presentation">
                 <h2>Présentation du site</h2>
-                <p>Sollicitudinsale dolorem maecenas alienum omnesque arcu varius menandri reformidans deserunt ad netus
-                    eos
-                    quem conclusionemque aperiri himenaeos. Suscipiantureos eos iudicabit iudicabit posse posidonium
-                    senectus turpis conubia ridiculus melius ornatus facilis arcu. Adhucaliquid faucibus molestie
-                    ancillae
-                    primis consetetur massa has sodales lectus iisque graecis cursus detracto nostrum. Promptasimilique
-                    contentiones senserit postea impetus dicant per nec accommodare eros fusce homero
-                    conclusionemque.</p>
+                <p>(The laws of roads don't apply anymore when 4:55 of Free Bird starts playing.)</p>
             </div>
-        </div>
+        </section>
     </div>
 </template>
 
@@ -74,7 +69,7 @@ export default {
                 if (!this.isAnimating) {
                     this.currentIndex = (this.currentIndex + 1) % this.techs.length;
                 }
-            }, 3000); // Change toutes les 3 secondes pour laisser le temps à l'animation
+            }, 3000);
         },
         beforeEnter() {
             this.isAnimating = true;
@@ -83,7 +78,7 @@ export default {
             this.isAnimating = false;
         },
         scrollToNextSection() {
-            const section = this.$refs.ouais;
+            const section = this.$refs.jsp;
             window.scrollTo({
                 top: section.offsetTop,
                 behavior: 'smooth'
