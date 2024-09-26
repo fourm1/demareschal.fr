42ef57fad160c5085abd0b54957058641ff332b2
ademareschal <toto.63146@gmail.com>
2024-09-26 16:55:51 +0200

    Singeries sur le footer

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,6 +2,7 @@
     <div id="app">
         <HeaderComponent :is-logged-in="isLoggedIn" :username="username" />
         <Home/>
+        <FooterComponent />
     </div>
 
 </template>
@@ -10,10 +11,12 @@
 
 import Home from "@/components/homepage/HomePageComponent.vue";
 import HeaderComponent from "@/components/header/HeaderComponent.vue";
+import FooterComponent from "@/components/footer/FooterComponent.vue";
 
 export default {
     name: 'App',
     components: {
+        FooterComponent,
         Home,
         HeaderComponent
     },
--- front/src/components/footer/FooterComponent.vue
+++ front/src/components/footer/FooterComponent.vue
@@ -1,7 +1,29 @@
 <template>
-
+    <footer class="footer">
+        <div class="footer-content">
+            <div class="social-icons">
+                <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                    <font-awesome-icon :icon="['fab', 'facebook']" />
+                </a>
+                <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                    <font-awesome-icon :icon="['fab', 'instagram']" />
+                </a>
+                <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+                    <font-awesome-icon :icon="['fab', 'linkedin']" />
+                </a>
+                <a href="https://github.com/fourm1" target="_blank" rel="noopener noreferrer">
+                    <font-awesome-icon :icon="['fab', 'github']" />
+                </a>
+                <a href="https://steamcommunity.com/id/fourm1ssou" target="_blank" rel="noopener noreferrer">
+                    <font-awesome-icon :icon="['fab', 'steam']" />
+                </a>
+            </div>
+        </div>
+    </footer>
 </template>
 
 <script setup>
+
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 
 </script>
