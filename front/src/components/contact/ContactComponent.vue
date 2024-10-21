c1589f31e93d580e206801e760d16618c03a22a6
ademareschal <toto.63146@gmail.com>
2024-10-21 18:02:26 +0200

    Formulaire dégueulasse - WIP

--- front/src/components/contact/ContactComponent.vue
+++ front/src/components/contact/ContactComponent.vue
@@ -1,6 +1,25 @@
 <template>
-    <div>
-        <h1>Contact</h1>
+    <div class="contact-form">
+        <h2>Contact</h2>
+        <form @submit.prevent="handleSubmit">
+            <div class="form-group">
+                <label for="firstname">Prénom</label>
+                <input type="text" id="lastname" v-model="firstname" required/>
+            </div>
+            <div class="form-group">
+                <label for="lastname">Nom</label>
+                <input type="text" id="lastname" v-model="lastname" required/>
+            </div>
+            <div class="form-group">
+                <label for="email">Courrier électronique</label>
+                <input type="email" id="email" v-model="email" required/>
+            </div>
+            <div class="form-group">
+                <label for="message">Message</label>
+                <textarea id="message" v-model="message" required></textarea>
+            </div>
+            <button type="submit">Envoyer le message</button>
+        </form>
     </div>
 </template>
 
@@ -8,6 +27,20 @@
 
 import '@/components/contact/contact.scss';
 
-export default {}
-
+export default {
+    data() {
+        return {
+            firstname: '',
+            lastname: '',
+            email: ''
+        };
+    },
+    methods: {
+        handleSubmit() {
+            console.log('Prénom: ', this.firstname);
+            console.log('Nom: ', this.lastname);
+            console.log('Courrier électronique: ', this.email);
+        }
+    }
+}
 </script>
